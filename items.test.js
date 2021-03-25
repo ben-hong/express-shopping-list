0f983bb6bc1025a31dff92731671c66e881a0b34
Ben Hong <benhong654@gmail.com>
2021-03-24 17:23:21 -0700

    finished up the tests

--- items.test.js
+++ items.test.js
@@ -24,4 +24,28 @@ describe('#items api', () => {
         expect(resp.statusCode).toEqual(200);
         expect(resp.body).toEqual({name: "popsicle", "price": 1.45});
     })
+
+    test('GET individual item fail', async function(){
+        const resp = await request(app).get('/items/dirt');
+        expect(resp.statusCode).toEqual(500);
+        expect(resp.body).toEqual({"error": {"status": 500}});
+    })
+
+    test('PATCH individual item', async function() {
+        const resp = await request(app).patch('/items/popsicle').send(
+            {
+                name: "new popsicle",
+                price: 999
+            }
+        );
+        expect(resp.statusCode).toEqual(200);
+        expect(resp.body).toEqual({updated: {name: "new popsicle", price: 999}});
+    })
+
+    test('DELETE individual item', async function() {
+        const resp = await request(app).delete('/items/popsicle')
+        expect(resp.statusCode).toEqual(200);
+        expect(resp.body).toEqual({message: "Deleted"});
+    })
+
 })
